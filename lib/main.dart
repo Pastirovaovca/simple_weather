397c0306a2fe69d07be0af7f75bab7f427976ee9
Marko <pastirova.ovca@gmail.com>
2021-08-30 00:09:45 +0200

    initial

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+// @dart=2.9
+import 'package:flutter/material.dart';
+import 'package:clima/screens/loading_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: LoadingScreen(),
+    );
+  }
+}
